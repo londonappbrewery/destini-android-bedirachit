d895e612cd2bbee178bcfec35015b2b0cd6ac39d
bedi.rachit <rachit.bedi1@gmail.com>
2017-10-13 14:03:28 +0530

    final commit

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,10 +2,17 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+   TextView mStoryTextView;
+    Button mTopButton;
+    Button mBottomButton;
+    int mStoryloc = 1;
 
 
     @Override
@@ -15,16 +22,82 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
-
+        mStoryTextView = (TextView) findViewById(R.id.storyTextView);
+        mTopButton = (Button) findViewById(R.id.buttonTop);
+        mBottomButton = (Button) findViewById(R.id.buttonBottom);
+        mStoryTextView.setText(R.string.T1_Story);
+        mTopButton.setText(R.string.T1_Ans1);
+        mBottomButton.setText(R.string.T1_Ans2);
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
 
 
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            updateStoryTop(mStoryloc);
 
+
+            }
+        });
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        mBottomButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+            updateStoryBottom(mStoryloc);
 
+
+            }
+
+        });
 
     }
+    private void updateStoryTop(int currentLoc){
+    if (currentLoc==1){
+        mStoryTextView.setText(R.string.T3_Story);
+        mTopButton.setText(R.string.T3_Ans1);
+        mBottomButton.setText(R.string.T3_Ans2);
+        mStoryloc = mStoryloc +1;
+
+    }else if  (currentLoc==2){
+          mStoryTextView.setText(R.string.T6_End);
+          mTopButton.setVisibility(View.GONE);
+          mBottomButton.setVisibility(View.GONE);
+        }else if (currentLoc==3){
+            mStoryTextView.setText(R.string.T3_Story);
+            mTopButton.setText(R.string.T3_Ans1);
+            mBottomButton.setText(R.string.T3_Ans2);
+            mStoryloc = mStoryloc +1;
+        }else {
+            mStoryTextView.setText(R.string.T6_End);
+            mTopButton.setVisibility(View.GONE);
+            mBottomButton.setVisibility(View.GONE);
+        }
+    }
+
+
+
+
+    private void  updateStoryBottom(int currentLoc){
+        if (currentLoc==1){
+            mStoryTextView.setText(R.string.T2_Story);
+            mTopButton.setText(R.string.T2_Ans1);
+            mBottomButton.setText(R.string.T2_Ans2);
+            mStoryloc = mStoryloc+2;
+    }else if (currentLoc==2){
+            mStoryTextView.setText(R.string.T5_End);
+            mTopButton.setVisibility(View.GONE);
+            mBottomButton.setVisibility(View.GONE);
+        }else if (currentLoc==3){
+            mStoryTextView.setText(R.string.T4_End);
+            mTopButton.setVisibility(View.GONE);
+            mBottomButton.setVisibility(View.GONE);
+
+        }else {
+            mStoryTextView.setText(R.string.T5_End);
+            mTopButton.setVisibility(View.GONE);
+            mBottomButton.setVisibility(View.GONE);
+        }
+}
 }
